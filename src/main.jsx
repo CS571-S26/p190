99384c81974ad215e5391000ef23479939553484
Ismael Jaral Vera <ijaralvera@wisc.edu>
2026-03-18 23:50:06 -0500

    published project to github pagesgit add .! :)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
 import { HashRouter } from 'react-router-dom';
